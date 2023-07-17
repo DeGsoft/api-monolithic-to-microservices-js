0f3b1b44eef20d8bec0e8b5b5f338aa8e95d0f04
DeGsoft <diegoezequielguillen@gmail.com>
2023-07-16 23:00:15 -0300

    [m2] feat (validateModel): validate model of endpoint

--- microservices/database/src/routes/index.js
+++ microservices/database/src/routes/index.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const store = require("../mongodb");
+const validateModel = require("../middlewares");
 
 const router = Router();
 
-router.get("/:model", async (req, res) => {
+router.get("/:model", validateModel, async (req, res) => {
   const { model } = req.params;
   const response = await store[model].list();
   res.status(200).json(response);
