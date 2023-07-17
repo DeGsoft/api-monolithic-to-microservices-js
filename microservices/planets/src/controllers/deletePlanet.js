8c9c454f56e0b9d1891aee395f312a828f377bcc
DeGsoft <diegoezequielguillen@gmail.com>
2023-07-17 02:20:47 -0300

    [m2] chore (planets): controllers

--- microservices/planets/src/controllers/deletePlanet.js
+++ microservices/planets/src/controllers/deletePlanet.js
@@ -2,7 +2,7 @@ const Planet = require("../data");
 const { response } = require("../utils");
 
 module.exports = async (req, res) => {
-  const { body } = req;
-  const newPlanet = await Planet.create(body);
-  response(res, 201, newPlanet);
+  const { id } = req.params;
+  const planet = await Planet.remove(id);
+  response(res, 202, planet);
 };
